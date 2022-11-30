54ac4e2d81b5391e7c4403286343deae8fd8e8ca
AAAAAfonso <75743252+AAAAAfonso@users.noreply.github.com>
2022-11-30 17:45:14 +0000

    big fix

--- player_src/src/udp_sender.h
+++ player_src/src/udp_sender.h
@@ -5,5 +5,5 @@ extern int socket_udp_fd;
 
 Result udp_sender_try_init();
 Result udp_sender_send(const u8 *data, size_t sz);
-Result udp_sender_recv(const u8 *data, size_t sz);
+Result udp_sender_recv(u8 *data, size_t sz);
 void udp_sender_fini();
